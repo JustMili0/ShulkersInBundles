38a8366acce8cad503c9bd08d107556c13c5ae02
JustMili <rodx160@gmail.com>
2026-01-20 20:38:16 +0100

    No errors but crashes in my neoforge pack so I guess needs some fixing

--- common/src/main/java/net/justmili/mixin/ShulkersInBundlesInsert.java
+++ common/src/main/java/net/justmili/mixin/ShulkersInBundlesInsert.java
@@ -2,17 +2,18 @@ package net.justmili.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.world.item.BundleItem;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.component.BundleContents;
+import net.minecraft.world.level.block.ShulkerBoxBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(BundleContents.Mutable.class)
-public class NoBundlesInBundles {
+public class ShulkersInBundlesInsert {
     @WrapOperation(method = "tryInsert", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/Item;canFitInsideContainerItems()Z"))
-    private boolean preventBundlesInBundles(Item item, Operation<Boolean> original) {
-        if (item instanceof BundleItem) {
+    private boolean shulkersinbundles(Item item, Operation<Boolean> original) {
+        if (item instanceof BlockItem blockItem && blockItem.getBlock() instanceof ShulkerBoxBlock) {
             return true;
         }
         return original.call(item);
--- common/src/main/java/net/justmili/mixin/ShulkersInBundlesWeight.java
+++ common/src/main/java/net/justmili/mixin/ShulkersInBundlesWeight.java
@@ -12,16 +12,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BundleContents.class)
-public class ShulkersInBundles {
-    @Inject(method = "canItemBeInBundle", at = @At("HEAD"), cancellable = true)
-    private static void allowShulkersInBundles(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-        if (stack.isEmpty()) return;
-        if (stack.getItem() instanceof BlockItem blockItem
-            && blockItem.getBlock() instanceof ShulkerBoxBlock) {
-
-            cir.setReturnValue(true);
-        }
-    }
+public class ShulkersInBundlesWeight {
     @Inject(method = "getWeight", at = @At("HEAD"), cancellable = true)
     private static void shulkerWeight(ItemStack stack, CallbackInfoReturnable<Fraction> cir) {
         if (stack.getItem() instanceof BlockItem blockItem
