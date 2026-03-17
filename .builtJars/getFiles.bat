717246a91e8725de76756c7678e6c6e4139ce21b
JustMili <rodx160@gmail.com>
2026-03-17 20:53:08 +0100

    1.2.0, public update after next PRs

--- .builtJars/getFiles.bat
+++ .builtJars/getFiles.bat
@@ -1,7 +1,6 @@
 @echo off
 echo Moving files...
 move ..\fabric\build\libs\*.jar . >nul
-move ..\quilt\build\libs\*.jar . >nul
 move ..\forge\build\libs\*.jar . >nul
 move ..\neoforge\build\libs\*.jar . >nul
 
