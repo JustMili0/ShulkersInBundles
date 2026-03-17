9660ade0a719db4c673e871068af6f6e9cc44f23
x3rt <git@x3rt.dev>
2026-03-17 15:47:11 -0600

    Prevent inserting items into bundles that are inside shulkers

---
+++ src/main/java/net/justmili/shulkersinbundles/mixin/NoInsertItemsInBundlesInShulkers.java
@@ -0,0 +1,25 @@
+package net.justmili.shulkersinbundles.mixin;
+
+import net.minecraft.world.entity.SlotAccess;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ClickAction;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.BundleItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.ShulkerBoxBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BundleItem.class)
+public class NoInsertItemsInBundlesInShulkers {
+    @Inject(method = "overrideOtherStackedOnMe", at = @At("HEAD"), cancellable = true)
+    private void preventItemsInBundlesInShulkers(ItemStack self, ItemStack other, Slot slot, ClickAction clickAction, Player player, SlotAccess slotAccess, CallbackInfoReturnable<Boolean> cir) {
+        if (clickAction == ClickAction.PRIMARY && !other.isEmpty()) {
+            if (slot.container instanceof ShulkerBoxBlockEntity) {
+                cir.setReturnValue(true);
+            }
+        }
+    }
+}
