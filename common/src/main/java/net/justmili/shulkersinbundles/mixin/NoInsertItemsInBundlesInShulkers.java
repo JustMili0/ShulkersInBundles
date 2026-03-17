56166fe97e85d4b2a08dd82980a0309dc5e55612
x3rt <git@x3rt.dev>
2026-03-17 15:43:49 -0600

    Remove abstract modifier

--- common/src/main/java/net/justmili/shulkersinbundles/mixin/NoInsertItemsInBundlesInShulkers.java
+++ common/src/main/java/net/justmili/shulkersinbundles/mixin/NoInsertItemsInBundlesInShulkers.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BundleItem.class)
-public abstract class NoInsertItemsInBundlesInShulkers {
+public class NoInsertItemsInBundlesInShulkers {
     @Inject(method = "overrideOtherStackedOnMe", at = @At("HEAD"), cancellable = true)
     private void preventItemsInBundlesInShulkers(ItemStack itemStack, ItemStack itemStack2, Slot slot, ClickAction clickAction, Player player, SlotAccess slotAccess, CallbackInfoReturnable<Boolean> cir) {
         if (clickAction == ClickAction.PRIMARY && !itemStack2.isEmpty()) {
