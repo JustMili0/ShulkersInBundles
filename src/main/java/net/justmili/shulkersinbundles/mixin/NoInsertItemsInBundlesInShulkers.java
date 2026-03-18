4c61149f28f69ea70a3db821524f667de8a9bb86
x3rt <git@x3rt.dev>
2026-03-18 11:44:33 -0600

    check slot type instead of container type
    - Turns out that the container type is sometimes SimpleContainer meaning that for 1 frame it appears as if it succeeded in inserting, and it would play the success sound

--- src/main/java/net/justmili/shulkersinbundles/mixin/NoInsertItemsInBundlesInShulkers.java
+++ src/main/java/net/justmili/shulkersinbundles/mixin/NoInsertItemsInBundlesInShulkers.java
@@ -3,10 +3,10 @@ package net.justmili.shulkersinbundles.mixin;
 import net.minecraft.world.entity.SlotAccess;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ClickAction;
+import net.minecraft.world.inventory.ShulkerBoxSlot;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.BundleItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.entity.ShulkerBoxBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,7 +17,7 @@ public class NoInsertItemsInBundlesInShulkers {
     @Inject(method = "overrideOtherStackedOnMe", at = @At("HEAD"), cancellable = true)
     private void preventItemsInBundlesInShulkers(ItemStack self, ItemStack other, Slot slot, ClickAction clickAction, Player player, SlotAccess slotAccess, CallbackInfoReturnable<Boolean> cir) {
         if (clickAction == ClickAction.PRIMARY && !other.isEmpty()) {
-            if (slot.container instanceof ShulkerBoxBlockEntity) {
+            if (slot instanceof ShulkerBoxSlot) {
                 cir.setReturnValue(true);
             }
         }
