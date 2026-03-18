6b1230c089c75f1420384ef4aa6fccbcf8248d7b
x3rt <git@x3rt.dev>
2026-03-18 11:55:37 -0600

    check slot-type instead of container

--- common/src/main/java/net/justmili/shulkersinbundles/mixin/NoInsertItemsInBundlesInShulkers.java
+++ common/src/main/java/net/justmili/shulkersinbundles/mixin/NoInsertItemsInBundlesInShulkers.java
@@ -3,10 +3,10 @@ package net.justmili.shulkersinbundles.mixin;
 import net.minecraft.world.entity.SlotAccess;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ClickAction;
+import net.minecraft.world.inventory.ShulkerBoxSlot;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.BundleItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.entity.ShulkerBoxBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class NoInsertItemsInBundlesInShulkers {
     @Inject(method = "overrideOtherStackedOnMe", at = @At("HEAD"), cancellable = true)
     private void preventItemsInBundlesInShulkers(ItemStack self, ItemStack other, Slot slot, ClickAction clickAction, Player player, SlotAccess slotAccess, CallbackInfoReturnable<Boolean> cir){
-        if (slot.container instanceof ShulkerBoxBlockEntity) {
+        if (slot instanceof ShulkerBoxSlot) {
             cir.setReturnValue(false);
         }
     }
